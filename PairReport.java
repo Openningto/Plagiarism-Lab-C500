c8c119faab71ffe55fd9231479329418df8e24b9
Dan <gwflagged@gmail.com>
2017-04-28 14:28:44 -0400

    Added PairReport + PlagiarismDetector (PD is unfinished)

--- PairReport.java
+++ PairReport.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
+
 public class PairReport {
     private String cleanerFile1;
     private String cleanerFile2;
-
-    private String file1;
-    private String file2;
-
     private Detector detector;
+
+    public PairReport(String file1, String file2, Detector dec) {
+        cleanerFile1 = file1;
+        cleanerFile2 = file2;
+        detector = dec;
+    }
 
     public String getCleaner1() {
         return cleanerFile1;
@@ -15,5 +19,23 @@ public class PairReport {
         return cleanerFile2;
     }
 
-    public
+    public String getString1() {
+        return detector.getString1();
+    }
+
+    public String getString2() {
+        return detector.getString2();
+    }
+
+    public double getSimilarity() {
+        return detector.getSimilarity();
+    }
+
+    public ArrayList<String> getCList() {
+        return detector.getCList();
+    }
+
+    public Detector getDetector() {
+        return detector;
+    }
 }
--- PlagiarismDetector.java
+++ PlagiarismDetector.java
@@ -1,13 +1,47 @@
+import java.io.File;
+
+
 public class PlagiarismDetector {
 
     private String submissions;
     private String sources;
-    private Detector detector;
 
-    public PlagiarismDetector(Detector det, String sub, String sou) {
-        detector = det;
+    final static double PLAGIARISM_THRESHOLD = 0.1;
+    private ArrayList<PairReport> reports;
+
+    public PlagiarismDetector(String sub, String sou) {
         submissions = sub;
         sources = sou;
+        reports = new ArrayList<>();
+        generateReports();
+    }
+
+    private void generateReports() {
+        File[] subFile = new File(submissions).listFiles();
+        File[] sourceFile
+    }
+
+    public ArrayList<PairReport> getAllPotentialOffenderrs( ){
+        ArrayList<PairReport> result = new ArrayList<PairReport>();
+
+        for (PairReport report : reports) {
+            if (report.getSimilarity() > PLAGIARISM_THRESHOLD) {
+                result.add(report);
+            }
+        }
+
+        return result;
+    }
+
+    public void printReports() {
+        ArrayList<PairReport> copy = new ArrayList<>(reports);
+        Collections.sort(copy);
+        System.out.println("----- PAIR REPORTS -----");
+        int count = 1;
+        for (PairReport report : copy) {
+            System.out.println(count+". Score: "+report.getSimilarity()+" | File 1: "+report.getCleaner1()+" | File 2: "+report.getCleaner2());
+            count++;
+        }
     }
 
     public List<String> getCList() {
